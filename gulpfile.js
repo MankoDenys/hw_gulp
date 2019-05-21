1b6c102aaaf7984469799f57e789f38819a7a5e4
MankoDenys <42086930+MankoDenys@users.noreply.github.com>
2019-05-21 08:49:13 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
-    // connect = require('gulp-connect');
-    sass = require('gulp-sass');
-    sourcemaps = require('gulp-sourcemaps');
+var config = {
+    appDir: './app',
+    sassPattern: 'scss/**/*.scss'
+};
 
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    csso = require('gulp-csso'),
+    connect = require('gulp-connect');
 
 // gulp.task('hello', function(callback) {
 //     console.log('Hello');
@@ -12,35 +18,33 @@ const gulp = require('gulp');
 // });
 
 gulp.task('sass', function () {
-    return gulp.src('./app/scss/**/*.scss')
+    return gulp.src([config.appDir+'/scss/main.scss', config.appDir+'/scss/style.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('main.css'))
+    .pipe(csso())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./app/css'));
 });
 
+gulp.task('connect', function() {
+    connect.server({
+      port: 8080
+    });
+    connect.serverClose();
+  });
+  
 // gulp.task('connect', function() {
-//     connect.server({
-//         root: [outputDir],
-//         port: 8080,
-//         keepalive: true,
-// 	open: { browser: 'chrome' }
-//     });
+//     connect.server();
+//   });
 
+// gulp.task('disconnect', function() {
+//     server.closeServer();
 // });
 
-// gulp.task('connect', function() {
-//     connect.server({
-//         root: 'SASS',
-//         livereload: true,
-//         open: true
-//       });
-// });
+gulp.task('watch', function () {
+    gulp.watch(config.appDir+'/'+config.sassPattern, gulp.parallel('sass', 'connect'))
+});
 
-// gulp.task('html', function() {
-//     gulp.src('./app/*.html')
-//     .pipe(gulp.dest('./app'))
-//     .pipe(connect.reload());
-// });
+gulp.task('default', gulp.parallel('sass', 'connect', 'watch'));
 
-// gulp.task('default', ['hello','connect']);
